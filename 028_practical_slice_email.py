d0ccfe60c46b599c7093f0dce6bc2c3cc81721c6
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2026-02-19 01:05:01 +0200

    practical slice email

--- 028_practical_slice_email.py
+++ 028_practical_slice_email.py
@@ -1,9 +1,15 @@
 # ---------------------------------------------
 # ------------- Practical Slice Email -------------
 # ---------------------------------------------
-
 sep = "#" * 60
 
-###############################################################
-print(sep)  ###################################################
-###############################################################
+theName = input("What's your name?").strip().capitalize()
+theEmail = input("What's your Email?").strip().lower()
+
+pos = theEmail.index("@")
+
+username = theEmail[:pos].capitalize()
+website = theEmail[pos + 1 :]
+
+print(f"Hello {theName} your email is {theEmail}")
+print(f"Your username is {username} \nYour website is {website}")
