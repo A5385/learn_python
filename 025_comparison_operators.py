2de068c2820be9bbab293d20f1ae9c2ccd92df56
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2026-02-18 23:09:53 +0200

    comparison operators

---
+++ 025_comparison_operators.py
@@ -0,0 +1,56 @@
+# ---------------------------------------------
+# ------------- Comparison Operators -------------
+# ---------------------------------------------
+# [1] ==  Equal
+# [2] !=  Not Equal
+# [3] >   Greater Than
+# [4] <   Less Than
+# [5] >=  Greeter Than or Equal
+# [6] <=  Less Than or Equal
+# ---------------------------------------------
+sep = "#"
+# [1] == Equal
+print(100 == 100)
+print(100 == 200)
+print(100 == 100.00)
+
+print(sep * 60)
+
+# [2] != Not Equal
+print(100 != 100)
+print(100 != 200)
+print(100 != 100.00)
+
+print(sep * 60)
+
+# [3] >    Greater Than
+print(100 > 100)
+print(100 > 200)
+print(100 > 100.00)
+print(100 > 40)
+
+print(sep * 60)
+
+# [4] <    Less Than
+print(100 < 100)
+print(100 < 200)
+print(100 < 100.00)
+print(100 < 40)
+
+print(sep * 60)
+
+# [5] >=   Greeter Than or Equal
+print(100 >= 100)
+print(100 >= 200)
+print(100 >= 100.00)
+print(100 >= 40)
+
+print(sep * 60)
+
+# [6] >=   Less Than or Equal
+print(100 <= 100)
+print(100 <= 200)
+print(100 <= 100.00)
+print(100 <= 40)
+
+print(sep * 60)
