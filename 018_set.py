7eb706b36b92d54b33424878ec74c70b35f00ec5
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2026-02-18 03:53:15 +0200

    set and set Methods

--- 018_set.py
+++ 018_set.py
@@ -4,8 +4,8 @@
 # [1] Set items are enclosed in curly braces
 # [2] Set items are not ordered and not indexed
 # [3] Set indexing and slicing cannot be done
-# [4]
-# [5]
+# [4] Set has only immutable data types (numbers, strings, tuples) list and dict are not
+# [5] Set Items is unique
 # -----------------------------
 
 # [1] Set items are enclosed in curly braces
@@ -18,3 +18,12 @@ print(mySetOne)
 # [3] Set indexing and slicing cannot be done
 mySetTwo = {1,2,3,4,5,6}
 #print(mySetTwo[0:3]) # Error => 'set' object is not subscriptable
+
+# [4] Set has only immutable data types (numbers, strings, tuples) list and dict are not
+# mySetThree = {"Osama",100,100.5,True,[1,2,3]} # Error => cannot use 'list' as a set element (unhashable type: 'list')
+mySetThree = {"Osama",100,100.5,True,(1,2,3)} 
+print(mySetThree) 
+
+# [5] Set Items is unique
+mySetFour= {1,2,"Osama","One","Osama",1}
+print(mySetFour) # will cancel 1 and Osama
