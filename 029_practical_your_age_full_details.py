7d7b543becc1e970f7f4584642ceea42344168f5
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2026-02-19 01:14:51 +0200

    practical you age full details

--- 029_practical_your_age_full_details.py
+++ 029_practical_your_age_full_details.py
@@ -1,7 +1,25 @@
 # ---------------------------------------------
 # ------ Practical Your Age Full Details ------
 # ---------------------------------------------
-sep = "#" * 60
-###############################################################
-print(sep)  ###################################################
-###############################################################
+
+# Input Age
+age = int(input("What's your age?").strip())
+
+print(age)
+print(type(age))
+
+# Get Age in all time units
+months = age * 12
+weeks = months * 4
+days = weeks * 7
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print("You live for:")
+print(f"{months} months.")
+print(f"{weeks:,} weeks.")
+print(f"{days:,} days.")
+print(f"{hours:,} hours.")
+print(f"{minutes:,} minutes.")
+print(f"{seconds:,} seconds.")
